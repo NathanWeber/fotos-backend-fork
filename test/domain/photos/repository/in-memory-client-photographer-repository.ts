b5785921144e60257efa4b2e52777aa9715b25ef
Nathan Weber <nathan.weber@ciss.com.br>
2024-10-22 10:40:24 -0300

    deleted client-photographer entity and repositorys

--- test/domain/photos/repository/in-memory-client-photographer-repository.ts
+++
@@ -1,4 +0,0 @@
-import type { ClientPhotographer } from '@/domain/photos/enterprise/entities/client-photographer'
-import { AbstractRepositoryImpl } from '@test/repositories/abstract-repository-impl'
-
-export class InMemoryClientPhotographerRepository extends AbstractRepositoryImpl<ClientPhotographer> {}
